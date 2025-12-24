1ec2de76a4dc123027e57fa47802e311e4d21861
25LP7865 25LP7865 <dhulan174@gmail.com>
2025-12-25 07:59:39 +0800

    namar

--- src/app/about/components/Upcoming.tsx
+++ src/app/about/components/Upcoming.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Movie } from "../page";
 import { MovieCard } from "./MovieCard";
 
@@ -32,6 +33,30 @@ export const Upcoming =() => {
     </div>
    )
 }
+=======
+import { ArrowRight } from "lucide-react";
+export type Upcoming = {
+  title: string;
+  poster: string;
+};
+
+const movieUpComing = async () => {
+  const response = await fetch("https://api.themoviedb.org/3/movie/upcoming", {
+    method: "GET",
+    headers: {
+      "Contact-Type": "application/json",
+      Authorization:
+        " Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjM1M2Y5ZTkyYzIyN2JiMmQzN2RiYTRlYzBlMDczNSIsIm5iZiI6MTc2NjUwNzU3My45MjYsInN1YiI6IjY5NGFjNDM1ODU0ZjE1YzI0ODdhYzkxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.suRvWWOi9bwOuzUKrSXdsWg6N144lg5xlmLAIsyznDk",
+    },
+  });
+  const data = await response.json();
+  return data.results;
+};
+
+export default async function Home() {
+  const movies: Upcoming[] = await movieUpComing();
+}
+>>>>>>> 18420f1 (Update Upcoming component)
 =======
 import { ArrowRight } from "lucide-react";
 export type Upcoming = {
